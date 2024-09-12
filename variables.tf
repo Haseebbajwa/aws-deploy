ce0969754e7e4adfa77c95d8d10a0fdd0f1f0b32
haseeb.ahmed <haseeb.ahmed@nordcloud.com>
2024-09-12 11:55:26 +0200

    region id fixed

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "AWS region to deploy resources in"
   type        = string  # Change to your desired region
-  default     = "us-west-1"
+  default     = "eu-west-1"
 }
 
 variable "lambda_role_name" {
