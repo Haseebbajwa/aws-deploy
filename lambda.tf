09e9da6e21ddebd99c9f2e1267aadc048a401954
haseeb.ahmed <haseeb.ahmed@nordcloud.com>
2024-09-12 13:10:59 +0200

    fixed lambda env var

--- lambda.tf
+++ lambda.tf
@@ -9,9 +9,9 @@ resource "aws_lambda_function" "uppercase_api" {
   runtime          = var.lambda_runtime
 
   # Environment variables if needed
-  environment {
-    variables = var.lambda_environment_variables
-  }
+  # environment {
+  #   variables = var.lambda_environment_variables
+  # }
 }
 resource "aws_cloudwatch_log_group" "lambda_log_group" {
   name = "/aws/lambda/${aws_lambda_function.uppercase_api.function_name}"
